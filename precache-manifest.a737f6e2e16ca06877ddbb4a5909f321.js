2507c76901dddd64fdae8b6379354f6dddbbb8c7
TuMko <mr.t09@mail.ru>
2020-08-26 10:08:38 +0300

    'add'

---
+++ precache-manifest.a737f6e2e16ca06877ddbb4a5909f321.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be88c3773b6ab679a58",
+    "url": "/vue-todolist/css/app.bd331103.css"
+  },
+  {
+    "revision": "c96f0dfff2baf00f7d60",
+    "url": "/vue-todolist/css/chunk-3a13e79c.d17cbb21.css"
+  },
+  {
+    "revision": "7811f0ad88f9f1f89135",
+    "url": "/vue-todolist/css/chunk-vendors.3e4956bb.css"
+  },
+  {
+    "revision": "1b2acb83ff6e28af886d64d1f81fdd70",
+    "url": "/vue-todolist/index.html"
+  },
+  {
+    "revision": "0be88c3773b6ab679a58",
+    "url": "/vue-todolist/js/app.fa0212e4.js"
+  },
+  {
+    "revision": "3c1d5b609830397e2f83",
+    "url": "/vue-todolist/js/chunk-2d0b64c8.991af2b7.js"
+  },
+  {
+    "revision": "b9496047592e7452e7b0",
+    "url": "/vue-todolist/js/chunk-2d21ef2c.12cebd06.js"
+  },
+  {
+    "revision": "c96f0dfff2baf00f7d60",
+    "url": "/vue-todolist/js/chunk-3a13e79c.e98bbfa6.js"
+  },
+  {
+    "revision": "7811f0ad88f9f1f89135",
+    "url": "/vue-todolist/js/chunk-vendors.8f184b97.js"
+  },
+  {
+    "revision": "b3b85f68868c6a9a99f3a3a2732ddb78",
+    "url": "/vue-todolist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todolist/robots.txt"
+  }
+]);
